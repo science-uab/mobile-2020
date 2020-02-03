51fd33ccfee45c009c658eaf1094f10f1c04cc7b
anamaria-sas <56155473+anamaria-sas@users.noreply.github.com>
2020-02-03 13:59:24 +0200

    Add files via upload

---
+++ Sas Anamaria/Assets/Scripts/Models/BallModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallModel : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    
+    private void Awake() {
+        if(rb == null)
+            rb = GetComponent<Rigidbody>();
+    }
+
+    public void Die(Vector3 restartPosition) {
+        rb.velocity = Vector3.zero;
+        transform.position = restartPosition;
+    }
+
+    public void Jump(Vector3 jumpPosition)
+    {
+        jumpPosition = this.transform.position;
+        jumpPosition.y += 0.2f;
+        this.transform.position = jumpPosition;
+    }
+
+    public void Move(Vector3 force) {
+        rb.AddForce(force , ForceMode.Acceleration);
+    }
+
+
+}
