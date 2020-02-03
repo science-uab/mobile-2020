2d43c0bba78cb78c7a3f767195bebd0cd433b44b
ioanaa1998 <60584786+ioanaa1998@users.noreply.github.com>
2020-02-03 13:45:38 +0200

    Add files via upload

---
+++ Jaravete Ioana/Jaravete Ioana-Cristina/file.js
@@ -0,0 +1,26 @@
+
+document.addEventListener("DOMContentLoaded", function(event) {
+if (window.DeviceOrientationEvent) {
+  window.addEventListener('deviceorientation', function(eventData) {
+    var tiltLR = eventData.gamma;
+    var tiltFB = eventData.beta;
+    var dir = eventData.alpha
+
+    deviceOrientationHandler(tiltLR, tiltFB, dir);
+  }, false);
+} else {
+  document.getElementById("notice").innerHTML = "DeviceOrientationEvent API nesuportat!"
+};
+
+    function deviceOrientationHandler(tiltLR, tiltFB, dir) {
+      document.getElementById("tiltLR").innerHTML = Math.ceil(tiltLR);
+      document.getElementById("tiltFB").innerHTML = Math.ceil(tiltFB);
+      document.getElementById("direction").innerHTML = Math.ceil(dir);
+      
+      var compassDisc = document.getElementById("compassDiscImg");
+      compassDisc.style.webkitTransform = "rotate("+ dir +"deg)";
+      compassDisc.style.MozTransform = "rotate("+ dir +"deg)";
+      compassDisc.style.transform = "rotate("+ dir +"deg)";
+    }
+
+});
